da85d0973c2d893362fbfe400fa49af35a763d73
Dylan Powers <ddp19@duke.edu>
2019-02-10 21:34:40 -0500

    Added search bar filtering functionality

--- public/core.js
+++ public/core.js
@@ -1,11 +1,33 @@
 var findAlums = angular.module('findAlums', []);
 
 findAlums.controller('mainController', ($scope, $http) => {
+  let JSONattributes = ['lastName', 'firstName', 'gradYear', 'position', 'company', 'committee', 'boardPosition', 'dukeEmail', 'email', 'linkedin'];
+  // this is a $scope object for table header
+  $scope.humanAttributes = ['Last Name', 'First Name', 'Graduation Year', 'Current Position', 'Current Company',
+                            'Committee(s)', 'Board Positions', 'Duke Email', 'Non-Duke Email', 'LinkedIn'];
+
+  // create attributes array which maps machine name to human name
+  $scope.attributes = JSONattributes.map((element, index) => {
+    return {
+      value: element,
+      humanName: $scope.humanAttributes[index]
+    }
+  });
+
   $http.get('/alums')
     .then((res) => {
-      $scope.data = res.data;
-      console.log(`Data is ${$scope.data}`);
-    }).catch((data) => {
-      console.log(`Error: ${data}`);
+      let rawData = res.data;
+      // transform data into objects for sorting
+      $scope.data = []; // will be array of person objects
+      rawData.forEach((person) => {
+        var personObject = {};
+        person.forEach((attribute, index) => {
+          let attrName = $scope.attributes[index].value;
+          personObject[attrName] = attribute;
+        });
+        $scope.data.push(personObject);
+      });
+    }).catch((err) => {
+      console.log(err);
     });
 });
